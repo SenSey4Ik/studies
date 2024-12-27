8b3dda67730bceeefa90c09b21cef5eff722055b
SenSey4Ik <massageinekb20@mail.ru>
2024-12-27 19:49:16 +0700

    Версия 3.0

--- cipher.py
+++ cipher.py
@@ -1,12 +1,11 @@
-import moduleproverka
-
-# Создаем русский алфавит
-rus_alphabet = 'абвгдеёжзийклмнопрстуфхцшщъыьэюяабвгдеёжзийклмнопрстуфхцшщъыьэюя'
+from moduleproverka import shifr
 
 
 #Предоставляем возможность выбрать расшифровку или зашифровку!
 choise = input("Введите 'расшифровать' или 'зашифровать': ")
 choise = choise.lower()
+
+shifr(choise)
 
 
 
